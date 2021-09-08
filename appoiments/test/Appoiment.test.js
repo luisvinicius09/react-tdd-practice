de97eae9cb73ba9f964b5255a772b24522f293b1
Luis Vinicius M <luisvinicius0906@gmail.com>
2021-09-07 22:09:21 -0300

    Add: new test, refactor code & test

--- appoiments/test/Appoiment.test.js
+++ appoiments/test/Appoiment.test.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appoiment } from '../src/Appoiment';
+
 /*
  * Defines the test suite(set of tests):
  * First argument = name of component, funcion or module
@@ -5,13 +9,34 @@
  */
 
 describe('Appoiments', () => {
-  it('renders the customer first name', () => {
-    // Each test can have as many expectations as you want
+  // Be careful of variables on the describe scope, they are not cleared between tests.
+  let customer;
+  let container;
 
-    expect(document.body.textContent).toMatch('Ashley');
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  const render = (component) => ReactDOM.render(component, container);
+
+  it('renders the customer first name', () => {
+    customer = { firstName: 'Ashley' };
+
+    // document.body.appendChild(container); // Append the container(div) to the document ❓❔
+
+    render(<Appoiment customer={customer} />);
+
+    // Each test can have as many expectations as you want
+    expect(container.textContent).toMatch('Ashley');
     // expect = Expectation
     // .toMatch = Matchers(there is a few of it)
   });
+
+  it('renders another customer first name', () => {
+    customer = { firstName: 'Jordan' };
+
+    render(<Appoiment customer={customer} />);
+
+    expect(container.textContent).toMatch('Jordan');
+  });
 });
-
-
