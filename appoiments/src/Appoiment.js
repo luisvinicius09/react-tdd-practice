42daf27239ea1f517c989d1f29a477577d7acdf3
Luis Vinicius M <luisvinicius0906@gmail.com>
2021-09-08 21:20:17 -0300

    Add AppoimentsDayView component with a div & ol

--- appoiments/src/Appoiment.js
+++ appoiments/src/Appoiment.js
@@ -3,3 +3,9 @@ import React from 'react';
 export const Appoiment = ({ customer: { firstName } }) => (
   <div>{firstName}</div>
 );
+
+export const AppoimentsDayView = () => (
+  <div id='appoimentsDayView'>
+    <ol></ol>
+  </div>
+);
